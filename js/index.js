e8a6a72f06bd8e73fde8468b23114fb9739c54bf
safkat07 <safkatsadli@gmail.com>
2023-09-02 04:58:00 +0600

    done without sort

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ const handleApi = async () => {
     const div = document.createElement("div");
     div.innerHTML = `
   <div class="tabs  tabs-boxed">      
-  <a  id="tab-button" onclick="handleLoadContent('${category.category_id}')" class="tab text-black  ">${category.category}</a>          
+  <a  id="tab-button" onclick="handleLoadContent('${category.category_id}')" class="tab text-black ">${category.category}</a>          
   </div>
 
     `;
@@ -27,18 +27,21 @@ const handleLoadContent = async (categoryID) => {
   const data = await response.json();
   console.log(data.data);
 
+
   const cardContainer = document.getElementById("card-container");
-  const emptyDivContainer = document.getElementById("empty-div")
+  const emptyDivContainer = document.getElementById("empty-div");
 
   cardContainer.innerHTML = "";
-  
+
   if (data.data.length == 0) {
     console.log("say array empty");
     const emptyDiv = document.createElement("div");
+    emptyDivContainer.innerHTML = "";
     emptyDiv.innerHTML = `
     <div class= "flex flex-col items-center justify-center" >
     <div>
-    <img class= "flex justify-center" src="./Icon.png" alt="">
+    <img class= "  flex justify-center" src="./Icon.png" alt="">
+    <p></p>
     </div>
    <div>
    <h2 class="lg:text-5xl   font-bold">
@@ -50,23 +53,32 @@ const handleLoadContent = async (categoryID) => {
     
     `;
     emptyDivContainer.appendChild(emptyDiv);
-    
+  } else {
+    emptyDivContainer.innerHTML = "";
   }
-  else{
-    emptyDivContainer.innerHTML = ''
-  }
-  
 
   data.data.forEach((content) => {
     const div = document.createElement("div");
+    const postedDate = content.others.posted_date
+    console.log(postedDate);const seconds = 3665; // Replace with your desired number of seconds
+    const result = secondsToHoursMinutes(postedDate);
+    // const finalShowHour = `${result.hours}hrs ${result.minutes} mins ago`
+    const finalShowHour = `${result.hours > 0 ? result.hours + 'hrs ' : ''}${result.minutes > 0 ? result.minutes + 'mins ago' : ''} `;
+    console.log(finalShowHour);
+    
     div.innerHTML = `
     
 
    <div class="grid justify-center ">
    
-   <figure><img class="w-72 mb-3  rounded-md h-44"  src=${
+   <figure class= "relative"><img  class="  w-72 mb-3  rounded-md h-44"  src=${
      content.thumbnail
    } alt="Shoes" /></figure>
+   <div class="absolute  text-white text-xs flex lg:ml-40 ml-52 mt-36 lg:mt-36"> 
+   <p class= " border-solid text-end  bg-gray-900  px-2 ">
+   ${finalShowHour}
+   </p>
+   </div>
   
     
     <div class="flex gap-2 mt-3 mb-10">
@@ -100,9 +112,32 @@ const handleLoadContent = async (categoryID) => {
 //callling the main api funciton
 handleApi();
 handleLoadContent("1000");
-// tab button color change
-const tabButton = document.getElementById("tab-button");
+//sort by view button
+const sortByView = (sortcategoryID) => {
+  console.log("sort by  button");
+};
 
-tabButton.addEventListener("click", function () {
-  tabButton.classList.add("tab-active");
-});
+// fetch data from api for second to hour
+// const showHourMinute = async (hourCategoryID) => {
+//   const response = await fetch(
+//     ` https://openapi.programming-hero.com/api/videos/category/${hourCategoryID}`
+//   );
+//   const data = await response.json();
+  
+//   console.log(result);
+// };
+
+// showHourMinute("1000");
+
+
+//seconds to hour
+
+function secondsToHoursMinutes(seconds) {
+  const hours = Math.floor(seconds / 3600); 
+  const minutes = Math.floor((seconds % 3600) / 60); 
+
+  return {
+    hours: hours,
+    minutes: minutes,
+  };
+}
