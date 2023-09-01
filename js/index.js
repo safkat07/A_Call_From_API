5511142589132ff1ffc49b4a605aa8d3775aa8bd
safkat07 <safkatsadli@gmail.com>
2023-09-02 02:45:57 +0600

    blog html section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 //creating the main api funtion
 const handleApi = async () => {
   const response = await fetch(
@@ -13,7 +12,7 @@ const handleApi = async () => {
     const div = document.createElement("div");
     div.innerHTML = `
   <div class="tabs  tabs-boxed">      
-  <a onclick="handleLoadContent('${category.category_id}')" class="tab text-black  ">${category.category}</a>          
+  <a  id="tab-button" onclick="handleLoadContent('${category.category_id}')" class="tab text-black  ">${category.category}</a>          
   </div>
 
     `;
@@ -27,8 +26,37 @@ const handleLoadContent = async (categoryID) => {
   );
   const data = await response.json();
   console.log(data.data);
+
   const cardContainer = document.getElementById("card-container");
+  const emptyDivContainer = document.getElementById("empty-div")
+
   cardContainer.innerHTML = "";
+  
+  if (data.data.length == 0) {
+    console.log("say array empty");
+    const emptyDiv = document.createElement("div");
+    emptyDiv.innerHTML = `
+    <div class= "flex flex-col items-center justify-center" >
+    <div>
+    <img class= "flex justify-center" src="./Icon.png" alt="">
+    </div>
+   <div>
+   <h2 class="lg:text-5xl   font-bold">
+   Oops!! Sorry, There is no content here
+   </h2>
+   </div>
+    
+    </div>
+    
+    `;
+    emptyDivContainer.appendChild(emptyDiv);
+    
+  }
+  else{
+    emptyDivContainer.innerHTML = ''
+  }
+  
+
   data.data.forEach((content) => {
     const div = document.createElement("div");
     div.innerHTML = `
@@ -36,19 +64,27 @@ const handleLoadContent = async (categoryID) => {
 
    <div class="grid justify-center ">
    
-   <figure><img class="w-72 mb-3  rounded-md h-44"  src=${content.thumbnail} alt="Shoes" /></figure>
+   <figure><img class="w-72 mb-3  rounded-md h-44"  src=${
+     content.thumbnail
+   } alt="Shoes" /></figure>
   
     
     <div class="flex gap-2 mt-3 mb-10">
     <div class="flex ">
-    <img class="w-8 h-8 rounded-full"  src=${content.authors[0].profile_picture} alt="Shoes" />
+    <img class="w-8 h-8 rounded-full"  src=${
+      content.authors[0].profile_picture
+    } alt="Shoes" />
     </div>
     <div class="flex flex-col">
     <h2 class="text-xl font-bold">${content.title}</h2>
     <h2 class="">${content.authors[0].profile_name}<span>verfiy</span></h2>
     <div class="flex gap-2">
     <h2>${content.others.views} views</h2>
-    <p>${content.authors[0].verified? '<img class="w-4 mt-1.5" src="./fi_10629607.jpg" alt="">' : ''} 
+    <p>${
+      content.authors[0].verified
+        ? '<img class="w-4 mt-1.5" src="./fi_10629607.jpg" alt="">'
+        : ""
+    } 
     </p>
     </div>
     
@@ -64,3 +100,9 @@ const handleLoadContent = async (categoryID) => {
 //callling the main api funciton
 handleApi();
 handleLoadContent("1000");
+// tab button color change
+const tabButton = document.getElementById("tab-button");
+
+tabButton.addEventListener("click", function () {
+  tabButton.classList.add("tab-active");
+});
